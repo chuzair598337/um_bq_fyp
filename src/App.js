0ef2d7975c94045030de35ce2bc961dfbb66e8bd
aroojfatimaaa4 <157275363+aroojfatimaaa4@users.noreply.github.com>
2024-08-13 19:00:29 +0500

    Recording button record on long press and send to Flask api

--- src/App.js
+++ src/App.js
@@ -9,31 +9,33 @@ import Bayan from "./pages/Bayan";
 import Tafheem from "./pages/Tafheem";
 import Tilawat from "./pages/Tilawat/Tilawat";
 import Recorder from "./Recorder/Recorder";
+import Recordbtn from "./Temp/recordingButton/Recordbtn";
 
 const App = () => {
   return (
-    <Router>
-      {/* Microphone recording*/}
-      <Recorder/>
-      {/* Responsive Navigation Bar */}
-      <Navbar />
+    // <Router>
+    //   {/* Microphone recording*/}
+    //   <Recorder/>
+    //   {/* Responsive Navigation Bar */}
+    //   <Navbar />
 
-      {/* Main Pages Content */}
-      <main className="wrapper">
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/reciter" element={<Reciter />} />
-            <Route path="/bayan" element={<Bayan />} />
-            <Route path="/tafheem" element={<Tafheem />} />
-            <Route path="/tilawat" element={<Tilawat />} />
-          </Routes>
-        </div>
-      </main>
+    //   {/* Main Pages Content */}
+    //   <main className="wrapper">
+    //     <div className="content">
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/reciter" element={<Reciter />} />
+    //         <Route path="/bayan" element={<Bayan />} />
+    //         <Route path="/tafheem" element={<Tafheem />} />
+    //         <Route path="/tilawat" element={<Tilawat />} />
+    //       </Routes>
+    //     </div>
+    //   </main>
 
       
 
-    </Router>
+    // </Router>
+    <Recordbtn></Recordbtn>
   );
 };
 
